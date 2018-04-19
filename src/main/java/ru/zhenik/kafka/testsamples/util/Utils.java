e36aa96a480d61b301c6993b1bc404ae1f53b72c
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-04-19 10:54:49 +0200

    fix utils + kafka

--- src/main/java/ru/zhenik/kafka/testsamples/util/Utils.java
+++ src/main/java/ru/zhenik/kafka/testsamples/util/Utils.java
@@ -1,6 +1,7 @@
 package ru.zhenik.kafka.testsamples.util;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -33,16 +34,23 @@ public class Utils {
   public static void main(String[] args) {
     Properties props = new Properties();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer"+ UUID.randomUUID().toString());
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
     KafkaProducer<String, String> kp = new KafkaProducer<>(props);
+    sendRecord("", "c,red", kp);
+    sendRecord("", "c,green", kp);
+    sendRecord("", "a,blue", kp);
+    sendRecord("", "b,green", kp);
+
+  }
+
+  public static void sendRecord(String key, String value, KafkaProducer<String, String> kp){
     kp.send(new ProducerRecord<String ,String>(
         FavouriteColors.FAVOURITE_COLOUR_INPUT,
-        "",
-        "c,green"
+        key,
+        value
     ));
-
   }
 }
